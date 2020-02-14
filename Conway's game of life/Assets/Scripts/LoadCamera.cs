54837f7c1a48fe617ee127f73671df0e68e67be7
Joshua Browne <joshua.browne@s.elim.school.nz>
2020-02-14 16:59:25 +1300

    Cell Changes and Zoom
    Made cell walls thicker and added a zoom function

--- Conway's game of life/Assets/Scripts/LoadCamera.cs
+++ Conway's game of life/Assets/Scripts/LoadCamera.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class LoadCamera : MonoBehaviour
 {
     Controller controller;
+    Camera view;
+    [SerializeField]
+    float zoomRate;
+    [SerializeField]
+    Vector2 scrollLimits;
+    [SerializeField]
+    float scrollSpeed;
     // Start is called before the first frame update
     void Start()
     {
+        view = GetComponent<Camera>();
         controller = Controller.controller;
         transform.position = new Vector3((controller.size.x) / 4f,( controller.size.y)/ 4f, -5);
     }
@@ -15,6 +23,22 @@ public class LoadCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        #region Scroll
+        float scroll = Input.GetAxis("MouseScrollWheel");
+        if (scroll > scrollSpeed)
+            scroll = scrollSpeed;
+        if (scroll < -scrollSpeed)
+            scroll = -scrollSpeed;
+
+        scroll *= view.orthographicSize;
+
+        view.orthographicSize += scroll * Time.deltaTime * zoomRate;
+        if(view.orthographicSize > scrollLimits.y)
+            view.orthographicSize = scrollLimits.y;
+        if (view.orthographicSize < scrollLimits.x)
+            view.orthographicSize = scrollLimits.x;
+
+        #endregion
+
     }
 }
